881bf15eacc2874bcab05bd382eff3500585b303
RMhoon <r_mhoon@hotmail.com>
2021-03-30 21:07:42 -0400

    Added code to compute Celcius and Fahrenheit

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Celsius to convert");
+            var cel = double.Parse(Console.ReadLine());
+            Console.WriteLine($"C {cel} is F:{TempConverter.CelsiusToFahrenheit(cel):#,###.#}");
+
+            Console.WriteLine("Enter Fahrenheit to convert");
+            var fahren = double.Parse(Console.ReadLine());
+            Console.WriteLine($"F {fahren}  is C:{TempConverter.FahrenheitToCelsius(fahren):#,###.#}");
+        }
+    }
+}
